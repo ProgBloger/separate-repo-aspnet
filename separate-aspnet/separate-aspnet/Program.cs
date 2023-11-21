9e806acb168e79c38000257f54a567d2560ac345
Mikhail Yakhyayeu <m.yakhyayeu@godeltech.com>
2023-11-21 23:02:25 +0100

    cors configuration

--- separate-aspnet/separate-aspnet/Program.cs
+++ separate-aspnet/separate-aspnet/Program.cs
@@ -6,6 +6,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowReactOrigins",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -15,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowReactOrigins");
 
 app.UseHttpsRedirection();
 
